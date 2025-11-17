6df9b04b1bbf648787e51e1e03b79d06c9cf20b1
JackieCheng1472 <90937850+JackieCheng1472@users.noreply.github.com>
2025-11-16 23:15:01 -0500

    cvsDataInitSqls

---
+++ Data/TravDB/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "Travel1" (
+    "TravelOptions" VARCHAR(255) PRIMARY KEY,
+    "dist" INTEGER,
+    "speed" INTEGER
+);
+
+CREATE TABLE "addr1" (
+    "ID" VARCHAR(255) PRIMARY KEY,
+    "address" INTEGER,
+    "income" NUMERIC
+);
---
+++ Data/TravDB2/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "Travel2" (
+    "TravelOptions" VARCHAR(255) PRIMARY KEY,
+    "dist" INTEGER,
+    "speed" INTEGER
+);
+
+CREATE TABLE "addr2" (
+    "ID" VARCHAR(255) PRIMARY KEY,
+    "address" INTEGER,
+    "income" NUMERIC
+);
+
