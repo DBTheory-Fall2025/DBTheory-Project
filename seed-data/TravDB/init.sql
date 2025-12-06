ecb8965bc77609deaa947a4c129474c47e51cf84
Jasmine-Mogadam <47947909+Jasmine-Mogadam@users.noreply.github.com>
2025-12-06 17:58:51 -0500

    move dbs to seed-data folder

--- seed-data/TravDB/init.sql
+++ seed-data/TravDB/init.sql
@@ -1,34 +1,11 @@
-CREATE TABLE users (
-    code INTEGER PRIMARY KEY,
-    company VARCHAR(255),
-    name VARCHAR(255),
-    gender VARCHAR(255),
-    age INTEGER
+CREATE TABLE "Travel1" (
+    "TravelOptions" VARCHAR(255) PRIMARY KEY,
+    "dist" INTEGER,
+    "speed" INTEGER
 );
 
-CREATE TABLE hotels (
-    "travelCode" INTEGER PRIMARY KEY,
-    "userCode" INTEGER,
-    name VARCHAR(255),
-    place VARCHAR(255),
-    days INTEGER,
-    price DECIMAL,
-    total DECIMAL,
-    date TIMESTAMP,
-    FOREIGN KEY("userCode") REFERENCES users(code)
-);
-
-CREATE TABLE flights (
-    "travelCode" INTEGER NULL,
-    "userCode" INTEGER,
-    "fromCity" VARCHAR(255),
-    "toCity" VARCHAR(255),
-    "flightType" VARCHAR(255),
-    price DECIMAL,
-    time DECIMAL,
-    distance DECIMAL,
-    agency VARCHAR(255),
-    date DATE,
-    FOREIGN KEY("userCode") REFERENCES users(code)
-    -- FOREIGN KEY("travelCode") REFERENCES hotels("travelCode") MATCH SIMPLE
+CREATE TABLE "addr1" (
+    "ID" VARCHAR(255) PRIMARY KEY,
+    "address" INTEGER,
+    "income" NUMERIC
 );
